1d70e1bcc6a73b181614ce3357c5ab8634e1abad
xzw <2907456397@qq.com>
2022-05-24 16:48:55 +0800

    添加事务
    Signed-off-by: xzw <2907456397@qq.com>

--- cloud-provider-payment8001/src/main/java/com/xzw/springCloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-payment8001/src/main/java/com/xzw/springCloud/service/impl/PaymentServiceImpl.java
@@ -3,19 +3,18 @@ package com.xzw.springCloud.service.impl;
 import com.xzw.springCloud.Mapper.PaymentMapper;
 import com.xzw.springCloud.entities.Payment;
 import com.xzw.springCloud.service.PaymentService;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
-//@Transactional//自己加的
 public class PaymentServiceImpl implements PaymentService {
     @Resource
     private PaymentMapper paymentMapper;
     @Override
+    @Transactional
     public int insert(Payment payment) {
         return paymentMapper.insert(payment);
     }
